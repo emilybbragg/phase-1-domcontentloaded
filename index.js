47d15c64ef095bf550ac1b8cec1f46692b71aeef
Emily <emilybribragg@gmail.com>
2021-12-07 11:49:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+//add event listener for dom content loaded
+document.addEventListener("DOMContentLoaded", () => {
+    let text = document.querySelector('p')
+    text.textContent = 'This is really cool!'
+  })
